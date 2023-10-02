2fa88eca508849ecef8625d4a14faab9c3a8496c
xarsh06 <114429858+xarsh06@users.noreply.github.com>
2023-10-02 13:27:55 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+def main():
+  #declaring the function in line 1
+ fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+  #using input for user to enter the temperature in fahrenheit and making fahrenheit variable
+ celsius = (fahrenheit - 32) * 5/9
+  #entering value of celcius variable 
+ print   (fahrenheit, "Fahrenheit is equal to", celsius, "Celsius.")
+  #telling python to print out a full sentence with celsius value
+main ()
+#printing function output
